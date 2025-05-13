7ee0c7e296473f806b00c1088132ec03aef49617
Jacob Liberty <jacob.tobin.liberty@gmail.com>
2025-05-13 17:25:41 -0400

    Fixing deployed CSS

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    output: 'export',
+    basePath: "/typetastic",
+    assetPrefix: "/typetastic/",
+    images: {
+      unoptimized: true,
+      dangerouslyAllowSVG: true,
+      contentDispositionType: 'attachment',
+      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    },
+  };
+  
+  module.exports = nextConfig;
